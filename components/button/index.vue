507aece467663a12e91f4adf90824ed0baa0a807
xuyadong <xuyadong@qiyi.com>
2018-08-23 19:39:45 +0800

    add components

---
+++ components/button/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="btn-container">Button</div>
+</template>
+
+<script>
+export default {
+  name: 'XButton',
+};
+</script>
+
+<style>
+  .btn-container {
+    color: aquamarine;
+    font-size: 24px;
+  }
+</style>
---
+++ components/header/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header-container">Header</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style>
+  .header-container {
+    color: aquamarine;
+    font-size: 24px;
+  }
+</style>
