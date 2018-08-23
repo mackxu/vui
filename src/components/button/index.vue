18b8c559100a96054ec9369e1589bc14dffd284e
xuyadong <xuyadong@qiyi.com>
2018-08-23 19:38:40 +0800

    add components

---
+++ src/components/button/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Button</div>
+</template>
+
+<script>
+export default {
+  name: 'XButton',
+};
+</script>
+
+<style>
+  .btn {
+    color: aquamarine;
+    font-size: 24px;
+  }
+</style>
