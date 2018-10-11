bcac3dc4514053672ed26ad94130a27c9cc3eaaa
xuyadong <xuyadong@qiyi.com>
2018-10-11 16:16:25 +0800

    优化组件名的挂载

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ const components = {
 
 const install = (Vue) => {
   Object.keys(components).forEach((name) => {
-    Vue.component(name, components[name]);
+    const comp = components[name];
+    console.log(comp.name || name);
+    // 优先用组件的name属性
+    Vue.component(comp.name || name, comp);
   });
 };
-
+// auto install
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue);
 }
