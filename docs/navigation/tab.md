2075591c56863b8bde96482c4d256052429635e1
xuyadong <xuyadong@qiyi.com>
2018-10-18 18:59:59 +0800

    完善演示代码及docs字段更新

--- docs/navigation/tab.md
+++ docs/navigation/tab.md
@@ -8,15 +8,17 @@
 |属性|说明|类型|默认值|
 |:--|:--|:--|:--|
 |type|标签的基本样式`vip`、`account`|string|vip|
-|activeKey|当前激活tab面板的 key|string|第一个tab|
+|value|当前激活tab面板的 key|string|第一个tab|
+|animated|是否使用 CSS3 动画|boolean|false|
+
+#### events
+|事件名|说明|返回值|
+|:--|:--|:---:|
+|on-click|用户点击切换标签页时触发||
 
 ### tab item
 #### props
 |属性|说明|类型|默认值|
 |:---|:---|:---:|:----:|
 |key|对应的`activeKey`|string|||
-|tab|tab头显示的文字|string|||
-#### events
-|事件名|说明|返回值|
-|:--|:--|:---:|
-|on-item-click|当前 tabItem 被点击时触发||
+|label|tab头显示的文字|string|||
--- examples/views/tab.vue
+++ examples/views/tab.vue
@@ -6,11 +6,26 @@
         <!--<x-tabitem>短信登录/注册</x-tabitem>-->
         <!--<x-tabitem>账号登录</x-tabitem>-->
       <!--</x-tab>-->
-      <Tabs :value.sync="tabIndex" @on-change="onChange">
+      <Tabs :value.sync="tabIndex" @on-click="onChange">
         <TabPane label="选项卡1">内容1</TabPane>
         <TabPane label="选项卡2">内容2</TabPane>
         <TabPane label="选项卡3">内容3</TabPane>
         <TabPane label="选项卡4">内容4</TabPane>
+      </Tabs>
+      <p></p>
+      <p>动画</p>
+      <Tabs :value.sync="tabIndex2" animated="true" @on-click="onChange">
+        <TabPane label="选项卡1">内容1</TabPane>
+        <TabPane label="选项卡2">内容2</TabPane>
+        <TabPane label="选项卡3">内容3</TabPane>
+        <TabPane label="选项卡4">内容4</TabPane>
+      </Tabs>
+      <p></p>
+      <p>type: account</p>
+      <p></p>
+      <Tabs type="account" :value.sync="tabIndex3" animated="true" @on-click="onChange">
+        <TabPane label="短信验证码登录">短信验证码登录</TabPane>
+        <TabPane label="账号登录">账号登录</TabPane>
       </Tabs>
     </div>
 </template>
@@ -21,6 +36,8 @@ export default {
   data() {
     return {
       tabIndex: 0,
+      tabIndex2: 0,
+      tabIndex3: 0,
     };
   },
   methods: {
