101d31a810b1decb94469d06762cc24d2c7d1644
xuyadong <xuyadong@qiyi.com>
2018-10-18 19:07:52 +0800

    完善演示代码

--- examples/views/tab.vue
+++ examples/views/tab.vue
@@ -1,29 +1,30 @@
 <template>
     <div>
-      <!--<x-tab :selectedIndex.sync="tabIndex" vip>-->
-        <!--<x-tabitem>短信登录/注册</x-tabitem>-->
-        <!--<x-tabitem>账号登录</x-tabitem>-->
-        <!--<x-tabitem>短信登录/注册</x-tabitem>-->
-        <!--<x-tabitem>账号登录</x-tabitem>-->
-      <!--</x-tab>-->
+      <Tabs>
+        <TabPane label="选项卡1">内容1</TabPane>
+        <TabPane label="选项卡2">内容2</TabPane>
+      </Tabs>
+      <hr>
+      <p> value 双向绑定 </p>
       <Tabs :value.sync="tabIndex" @on-click="onChange">
         <TabPane label="选项卡1">内容1</TabPane>
         <TabPane label="选项卡2">内容2</TabPane>
         <TabPane label="选项卡3">内容3</TabPane>
-        <TabPane label="选项卡4">内容4</TabPane>
       </Tabs>
       <p></p>
+      <hr>
       <p>动画</p>
-      <Tabs :value.sync="tabIndex2" animated="true" @on-click="onChange">
+      <Tabs :animated="true" @on-click="onChange">
         <TabPane label="选项卡1">内容1</TabPane>
         <TabPane label="选项卡2">内容2</TabPane>
         <TabPane label="选项卡3">内容3</TabPane>
         <TabPane label="选项卡4">内容4</TabPane>
       </Tabs>
       <p></p>
+      <hr>
       <p>type: account</p>
       <p></p>
-      <Tabs type="account" :value.sync="tabIndex3" animated="true" @on-click="onChange">
+      <Tabs type="account" @on-click="onChange">
         <TabPane label="短信验证码登录">短信验证码登录</TabPane>
         <TabPane label="账号登录">账号登录</TabPane>
       </Tabs>
